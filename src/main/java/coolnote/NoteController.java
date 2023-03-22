7c823b83920dcb88dc569afcb0d0c5c750c7861f
cipidee <enikbodnar@gmail.com>
2023-03-22 13:33:02 +0100

    commit

---
+++ src/main/java/coolnote/NoteController.java
@@ -0,0 +1,38 @@
+package coolnote;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
+public class NoteController {
+    static String note;
+    static int editCount = 0;
+
+    @PostMapping(value = "note")
+    public String processAddNoteForm(@RequestParam String newNote) {
+        note = newNote;
+        // Redirect to "/"
+        return "redirect:";
+    }
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("note", note);
+        model.addAttribute("editCount", editCount);
+
+        return "index";
+    }
+
+    @GetMapping(value = "note")
+    public String displayAddNoteForm(Model model) {
+        model.addAttribute("title", editCount == 0 ? "Add Note" : "Edit Note");
+        model.addAttribute("note", note);
+        ++editCount;
+        return "note";
+    }
+}
